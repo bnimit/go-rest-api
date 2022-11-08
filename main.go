444e4280bba3eac8b85e14f17f04a0f8fcd5f46b
Nimit Bhandari <nimitbhandari@outlook.com>
2022-11-08 21:43:37 +0530

    Refactor the code to make it look better

--- main.go
+++ main.go
@@ -18,43 +18,48 @@ var books = []Book{
 	{ID: "3", Title: "The Wizard Of Oz", Author: "L. Frank Baum"},
 }
 
+func listBooksHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
+
+func createBookHandler(c *gin.Context) {
+	var book Book
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	books = append(books, book)
+	c.JSON(http.StatusCreated, book)
+}
+
+func deleteBookHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, a := range books {
+		if a.ID == id {
+			books = append(books[:i], books[i+1:]...)
+			break
+		}
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
 func main() {
 	r := gin.New()
 
 	// GET Request
-	r.GET("/books", func(c *gin.Context) {
-		c.JSON(http.StatusOK, books)
-	})
+	r.GET("/books", listBooksHandler)
 
 	// POST Request
-	r.POST("/books", func(c *gin.Context) {
-		var book Book
-
-		if err := c.ShouldBindJSON(&book); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		books = append(books, book)
-		c.JSON(http.StatusCreated, book)
-
-	})
+	r.POST("/books", createBookHandler)
 
 	// DELETE Request
-	r.DELETE("/books/:id", func(c *gin.Context) {
-		id := c.Param("id")
-
-		for i, a := range books {
-			if a.ID == id {
-				books = append(books[:i], books[i+1:]...)
-				break
-			}
-		}
-
-		c.Status(http.StatusNoContent)
-	})
+	r.DELETE("/books/:id", deleteBookHandler)
 
 	r.Run()
 }
